7398949de3da926e6a9390b642deec2aa9318e40
Kollivolli <120576766+Kollivolli@users.noreply.github.com>
2022-12-14 16:47:04 +0200

    Update guessinggame.bat

--- guessinggame.bat
+++ guessinggame.bat
@@ -1,28 +1,28 @@
 @echo off
 color 0e
-title Guessing Game by seJma
+title Ära arvamis mäng by Nitrous1M
 set /a guessnum=0
-set /a answer=%RANDOM%
+set /a answer=%1-1000%
 set variable1=surf33
 echo -------------------------------------------------
-echo Welcome to the Guessing Game! 
+echo Arvamismäng! 
 echo. 
-echo Try and Guess my Number! 
+echo Proovi ära arvata arv! 
 echo -------------------------------------------------
 echo. 
 :top
 echo. 
 set /p guess=
 echo. 
-if %guess% GTR %answer% ECHO Lower! 
-if %guess% LSS %answer% ECHO Higher! 
+if %guess% GTR %answer% ECHO Madalam! 
+if %guess% LSS %answer% ECHO Kõrgem! 
 if %guess%==%answer% GOTO EQUAL
 set /a guessnum=%guessnum% +1
-if %guess%==%variable1% ECHO Found the backdoor hey?, the answer is: %answer%
+if %guess%==%variable1% ECHO Leidsid tagaukse?, vastus on: %answer%
 goto top
 :equal
-echo Congratulations, You guessed right!!! 
+echo Juhhuu, sa arvasid arvu ära!!! 
 echo. 
-echo It took you %guessnum% guesses. 
+echo See võttis sul %guessnum% arvamist. 
 echo. 
 pause
